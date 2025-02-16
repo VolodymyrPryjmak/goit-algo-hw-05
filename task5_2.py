fcc946481c05783a53da8621141f0db771814174
Volodymyr Pryjmak <v_pryjmak@ukr.net>
2025-02-16 17:03:28 +0200

    Add files via upload

---
+++ task5_2.py
@@ -0,0 +1,23 @@
+from typing import Callable
+import re
+
+def sum_profit(text: str) -> Callable[[str], float]:
+    
+    total_income = 0
+    def generator_numbers(text: str):
+        list_txt_dig = re.findall(r"\d+\.\d+", text) # числа з точкою (float)
+        list_txt2 = re.split(" ",text)            
+        list_txt2 = [elem for elem in list_txt2 if elem.isdigit()] # числа без точки
+        list_txt_dig.extend(list_txt2)
+        for el in list_txt_dig:
+            yield float(el)    
+        return 
+    
+    for el in generator_numbers(text):
+        total_income += el
+
+    return total_income
+
+text = "Загальний дохід працівника складається з декількох частин: 1000 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.0 доларів."
+total_income = sum_profit(text)
+print(f"Загальний дохід: {total_income}")
