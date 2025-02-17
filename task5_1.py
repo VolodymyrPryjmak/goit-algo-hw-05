dead8fa94c1f1f69bdc1528b4a760a9e1922e98e
Volodymyr Pryjmak <v_pryjmak@ukr.net>
2025-02-17 21:23:51 +0200

    Add files via upload

--- task5_1.py
+++ task5_1.py
@@ -2,20 +2,20 @@ from typing import Callable #, Dict
 
 def caching_fibonacci() -> Callable[[int], int]:
     cache = {}
-    def fibonacci(n: int) -> int:
-#        print(cache)  
-        for i in range(n+1):
-            if i in cache:
-                pass
-            else: 
-                if i == 0:
-                   cache[i] = 0
-                elif i == 1:   
-                   cache[i] = 1
-                else:   
-                   cache[i] = cache[i-2] + cache[i-1]
 
-        return cache[n]
+    def fibonacci(n: int) -> int:
+        if n in cache:
+           return cache[n]
+        
+        if n == 0:
+           cache[n] = n
+           return cache[n]
+        elif n == 1:
+           cache[n] = n
+           return cache[n]
+        else:    
+            cache[n] = fibonacci(n-2) + fibonacci(n-1)
+            return cache[n]  
 
     return fibonacci
 
