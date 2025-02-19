6a73faa54c2d5ad22e8d3b1bd3ecc6009128d4ff
Volodymyr Pryjmak <v_pryjmak@ukr.net>
2025-02-19 20:34:04 +0200

    Add files via upload

--- task5_3.py
+++ task5_3.py
@@ -26,30 +26,31 @@ def filter_logs_by_level(logs: list, level: str) -> list:
     return
 
 def count_logs_by_level(logs: list) -> dict:
-    kl_level = collections.Counter(logs)
+    el_level = []
+    for el in logs:
+        el_level.append(el["level"])
+    kl_level = collections.Counter(el_level)
     return kl_level
 
 def parse_log_line(line: str) -> dict:
-    log_lines = []
-    #level = set()
-    level_all = []
-    for elem in line:
-        el = re.split(' ',elem.strip())
-        poz_message = len(el[0].strip()) + len(el[1].strip()) + len(el[2].strip()) + 3
-        log_lines.append({"data": el[0], "time": el[1], "level": el[2], "message": elem[poz_message:].strip()})
-        #level.add(el[2])   
-        level_all.append(el[2])
-    return log_lines,  count_logs_by_level(level_all) 
+    el = re.split(' ',line)
+    poz_message = len(el[0].strip()) + len(el[1].strip()) + len(el[2].strip()) + 3
+    el_line = {"data": el[0], "time": el[1], "level": el[2], "message": line[poz_message:].strip()}
+    return el_line
 
 def load_logs(file_path: str) -> list:
     with open(file_path, "r") as file:
-        return parse_log_line(file.readlines())    
+         log_lines = []
+         for el in file.readlines():
+             log_lines.append(parse_log_line(el.strip()))  # for el in file.readlines():
+         return log_lines    
 
 def main():
     if len(sys.argv) > 1:
         logfile = Path(sys.argv[1])
         if logfile.exists():
-           log_lines, kl_level = load_logs(logfile)
+           log_lines = load_logs(logfile)
+           kl_level = count_logs_by_level(log_lines)
            display_log_counts(kl_level)
            if len(sys.argv) == 3:
               if sys.argv[2].upper() in kl_level:
@@ -65,4 +66,4 @@ def main():
 if __name__ == "__main__":
      main()
 # 
-#    python task5_3.py  logfile.log 
+#    python task5_3.py  logfile.log debug
